96b3b1abedb44ebbc1b27069524b0b4e82e2659d
mkannapally <akannapa@chicagobooth.edu>
2022-10-20 20:50:33 +0000

    add is fully working!

--- app/controllers/omnicalc_controller.rb
+++ app/controllers/omnicalc_controller.rb
@@ -3,6 +3,15 @@ class OmnicalcController < ApplicationController
     render({:template => "omnicalc/add.html.erb"})
   end
 
+  def wizard_add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    
+    @addition = @first_num + @second_num
+
+    render({:template => "omnicalc/wizard_add.html.erb"})
+  end
+
   def subtract
     render({:template => "omnicalc/subtract.html.erb"})
   end
@@ -14,5 +23,5 @@ class OmnicalcController < ApplicationController
   def divide
     render({:template => "omnicalc/divide.html.erb"})
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
 
   get("/add" , {:controller => "omnicalc" , :action => "add"})
+  get("/wizard_add" , {:controller => "omnicalc" , :action => "wizard_add"})
+
   get("/subtract" , {:controller => "omnicalc" , :action => "subtract"})
   get("/multiply" , {:controller => "omnicalc" , :action => "multiply"})
   get("/divide" , {:controller => "omnicalc" , :action => "divide"})
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
